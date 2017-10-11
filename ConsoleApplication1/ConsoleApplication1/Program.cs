7cd7b3db25545267efa459b13a7f499736027b9e
timothychan986 <32720097+timothychan986@users.noreply.github.com>
2017-10-11 23:36:37 +0100

    added RDotNet

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using RDotNet;
 
 namespace ConsoleApplication1
 {
@@ -10,6 +7,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            RService();
+
             //print out and read line 
             Console.WriteLine("Enter input:");
 
@@ -21,6 +20,26 @@ namespace ConsoleApplication1
             //useful command eg. R
 
             //try to run R in C#
+        }
+
+        static void RService()
+        {
+            REngine.SetEnvironmentVariables(); // <-- May be omitted; the next line would call it.
+            REngine engine = REngine.GetInstance();
+
+            // A somewhat contrived but customary Hello World:
+            var e = engine.Evaluate("x <- 3");
+            NumericVector x = engine.GetSymbol("x").AsNumeric();
+            engine.Evaluate("y <- 1:10");
+            NumericVector y = engine.GetSymbol("y").AsNumeric();
+
+            CharacterVector charVec = engine.CreateCharacterVector(new[] { "Hello, R world!, .NET speaking" });
+            engine.SetSymbol("greetings", charVec);
+            engine.Evaluate("str(greetings)"); // print out in the console
+            string[] a = engine.Evaluate("'Hi there .NET, from the R engine'").AsCharacter().ToArray();
+            Console.WriteLine("R answered: '{0}'", a[0]);
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
         }
     }
 }

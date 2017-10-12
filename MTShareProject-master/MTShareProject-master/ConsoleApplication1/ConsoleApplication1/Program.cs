5616b13e1171a7634c07956516332f17bb3cc6d0
timothychan986 <32720097+timothychan986@users.noreply.github.com>
2017-10-12 10:28:31 +0100

    r change and refactor

---
+++ MTShareProject-master/MTShareProject-master/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input;
+            RService rService = new RService();
+
+            do
+            {
+                Console.WriteLine("\r\nEnter R code:");
+                input = Console.ReadLine();
+
+                rService.RunCode(input);
+            }
+            while (!string.IsNullOrEmpty(input));
+
+        }
+    }
+}
---
+++ MTShareProject-master/MTShareProject-master/ConsoleApplication1/ConsoleApplication1/RService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using RDotNet;
+
+namespace ConsoleApplication1
+{
+    public class RService
+    {
+        private readonly REngine _rEngine;
+
+        public RService()
+        {
+            REngine.SetEnvironmentVariables();
+            _rEngine = REngine.GetInstance();
+        }
+
+        public void RunCode(string input)
+        {
+            try
+            {
+                var e = _rEngine.Evaluate(input); //result from R code will be print out
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(exception);
+                //throw;
+            }
+
+            string[] a = _rEngine.Evaluate("'from the R.NET engine'").AsCharacter().ToArray();
+        }
+
+        //static void RService()
+        //{
+        //    REngine.SetEnvironmentVariables();
+        //    REngine engine = REngine.GetInstance();
+
+        //    var e = engine.Evaluate("x <- 3"); //result from R code will be print out
+        //    NumericVector x = engine.GetSymbol("x").AsNumeric();
+
+        //    engine.Evaluate("y <- 1:10");
+        //    NumericVector y = engine.GetSymbol("y").AsNumeric();
+
+        //    string[] a = engine.Evaluate("'Hi there .NET, from the R engine'").AsCharacter().ToArray();
+        //    Console.WriteLine("R answered: '{0}'", a[0]);
+
+        //    Console.WriteLine("Press any key to exit the program");
+        //    Console.ReadKey();
+        //}
+    }
+}
